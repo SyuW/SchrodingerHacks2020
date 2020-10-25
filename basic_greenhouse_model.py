596323437d2e4576c87ad7618960004beca066ad
Sam Yu <s249yu@edu.uwaterloo.ca>
2020-10-25 15:43:55 -0400

    Test of consistency with Wikipedia figure

--- basic_greenhouse_model.py
+++ basic_greenhouse_model.py
@@ -16,13 +16,24 @@ Extensions for introducing realism/complexity:
 S_0 = 1366 #W * m^(-2)
 sb = 5.67 * 10 ** (-8)
 albedo = 0.3
-emv = 1 #emissivity
+emv = 0.78 #emissivity
 
 
 def main():
     emv_factor = 2 / (2 - emv)
-    T_eff = (S_0*(1-albedo)/(4*sb)) ** 0.25 # Absence of atmos.
+    T_eff = (S_0*(1-albedo)/(4*sb)) ** 0.25 # Absence of atmosphere
     T_s =  T_eff * (emv_factor) ** 0.25
+    T_a =  T_s * (2 ** (-0.25))
+
+    # In SI flux units W*m^(-2):
+    solar_flux = (1/4) * S_0
+    surface_flux = sb * (T_s) ** 4
+    greenhouse_flux = emv * sb * T_a ** 4
+    planetary_flux = (1 - emv)*sb*T_s**4 + emv*sb*T_a**4
+
+    # Percentages should be roughly 100, 115, 45, 70 as per the wikipedia article
+    print(np.array([solar_flux, surface_flux, greenhouse_flux, planetary_flux]) * (100 / solar_flux))
+    
     return T_s
 
 
