5f57fac3cbdc421f6301001e4407340857f0a042
Sam Yu <s249yu@edu.uwaterloo.ca>
2020-10-26 21:05:07 -0400

    a little cleaning up, different radii/speeds for ground/excited states

--- random_motion.py
+++ random_motion.py
@@ -3,10 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+GROUND_STATE_MAX_RADIUS = 0.2; EXCITED_STATE_MAX_RADIUS = 0.5
+GROUND_SPEED = 0.1; EXCITED_SPEED = 0.2
+
+
 class Molecule():
 
     def find_next_position(self):
-        r = np.random.uniform(0, 1)
+        r = np.random.uniform(0, self.rmax)
         theta = np.random.uniform(0, 2*np.pi)
 
         return np.array([r*np.cos(theta), r*np.sin(theta)])
@@ -17,7 +21,7 @@ class Molecule():
         if dist > 0.1:
             self.temp.remove()
             self.fig.canvas.draw_idle()
-            self.curr_pos += self.displ*0.05
+            self.curr_pos += self.displ * self.speed
             self.temp, = plt.plot(*self.curr_pos, 'ro') 
         
         else:
@@ -25,9 +29,9 @@ class Molecule():
             self.displ = self.next_pos - self.curr_pos
 
     def excited_state(self):
-        self.speed = 8
+        self.speed = 3
 
-    def __init__(self):
+    def __init__(self, excited):
         self.fig = plt.figure()
         self.ax = self.fig.add_axes([0, 0, 1, 1], frameon=True)
         
@@ -38,8 +42,12 @@ class Molecule():
         self.next_pos = np.array([0., 0.])
         self.displ = self.curr_pos - self.next_pos
 
-        self.speed = 2
-        self.excited = False
+        if excited:
+            self.rmax = EXCITED_STATE_MAX_RADIUS
+            self.speed = EXCITED_SPEED
+        else:
+            self.rmax = GROUND_STATE_MAX_RADIUS
+            self.speed = GROUND_SPEED
 
         self.temp, = plt.plot(*self.curr_pos, 'ro') 
         animation = FuncAnimation(self.fig, self.update, interval=1)
@@ -47,4 +55,4 @@ class Molecule():
 
 
 if __name__ == "__main__":
-    foo = Molecule()
+    foo = Molecule(excited=True)
