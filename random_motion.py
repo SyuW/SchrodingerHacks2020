b6143d3a07022e55000d2bd7e07baab09a970a8d
Sam Yu <s249yu@edu.uwaterloo.ca>
2020-10-26 20:41:31 -0400

    Random motion for one molecule

--- random_motion.py
+++ random_motion.py
@@ -12,17 +12,17 @@ class Molecule():
         return np.array([r*np.cos(theta), r*np.sin(theta)])
 
     def update(self, i):
+        dist = np.linalg.norm(self.curr_pos - self.next_pos)
 
-        if np.linalg.norm(diff) > 0.1:
-            self.curr_pos += self.displ*0.01
-            plt.plot(*diff, 'ro')
+        if dist > 0.1:
+            self.temp.remove()
+            self.fig.canvas.draw_idle()
+            self.curr_pos += self.displ*0.05
+            self.temp, = plt.plot(*self.curr_pos, 'ro') 
         
         else:
-            self.finish_pos = self.find_next_position()
-            self.displ = self.curr_pos - self.finish_pos
-        
-        plt.gca().get_xaxis().set_ticks([])
-        plt.gca().get_yaxis().set_ticks([])
+            self.next_pos = self.find_next_position()
+            self.displ = self.next_pos - self.curr_pos
 
     def excited_state(self):
         self.speed = 8
@@ -33,12 +33,16 @@ class Molecule():
         
         self.ax.set_xlim(-2, 2)
         self.ax.set_ylim(-2, 2)
-        self.curr_pos = np.array([0, 0])
-        self.next_pos = np.array([0, 0])
+
+        self.curr_pos = np.array([0., 0.])
+        self.next_pos = np.array([0., 0.])
+        self.displ = self.curr_pos - self.next_pos
+
         self.speed = 2
         self.excited = False
 
-        animation = FuncAnimation(self.fig, self.update, interval=10)
+        self.temp, = plt.plot(*self.curr_pos, 'ro') 
+        animation = FuncAnimation(self.fig, self.update, interval=1)
         plt.show()
 
 
